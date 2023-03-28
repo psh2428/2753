0f0177d9b304bf5e0ca03fb1c43a4c1dbf1cfb99
psh2428 <simon2428@naver.com>
2023-03-28 12:38:12 +0900

    초기 커밋.

---
+++ 2753/2753.cpp
@@ -0,0 +1,25 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+void main() {
+	int year;
+	scanf("%d", &year);
+	if (year < 1 && year >4000) {
+		printf("값을 잘못 입력하였습니다.");
+		return;
+	}
+	else if (year % 4 == 0 && year % 100 != 0) {
+		printf("1");
+	}
+	else if (year % 400 == 0) {
+		printf("1");
+	}
+	else {
+		printf("0");
+	}
+	
+
+
+
+	
+}
